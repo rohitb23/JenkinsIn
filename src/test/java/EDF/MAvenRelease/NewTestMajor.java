0284f4aae5250a00c1d809313d3540982f32ed4f
rohit <rbaweja2311@gmail.com>
2019-06-05 12:41:08 +0530

    First Commit

---
+++ src/test/java/EDF/MAvenRelease/NewTestMajor.java
@@ -0,0 +1,46 @@
+package EDF.MAvenRelease;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import junit.framework.Assert;
+
+public class NewTestMajor {
+	
+	public WebDriver driver;
+  @Test
+  public void f() {
+	  
+	  driver.get("https://www.google.com");
+	//  System.out.println(driver.getCurrentUrl());
+	  Assert.assertEquals("Checking Title","Google", driver.getTitle());
+  }
+  
+  
+  @BeforeClass
+  public void ChromeSetup() {
+
+		WebDriverManager.chromedriver().setup();
+		  driver = new ChromeDriver();
+		/*driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());*/
+		System.out.println("Driver is up");
+		
+	  
+	  
+	  
+  }
+  
+  
+  @AfterClass
+  public void ClosingInstance() {
+	  System.out.println("Closing Drivers");
+	  driver.quit();
+  }
+  
+  
+}
---
+++ src/test/java/EDF/MAvenRelease/TestIntegration.java
@@ -0,0 +1,25 @@
+package EDF.MAvenRelease;
+
+import javax.sql.rowset.WebRowSet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+public class TestIntegration {
+
+	
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		 WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		driver.quit();
+		
+	}
+	
+}
